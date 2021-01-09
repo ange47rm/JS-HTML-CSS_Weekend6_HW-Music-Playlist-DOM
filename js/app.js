aeb27aa6acc70366becf2e1e09b6f6ca5751a8fb
ange47rm <ange47rm@hotmail.com>
2021-01-09 12:35:17 +0000

    Added JS script to HTML

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener( 'DOMContentLoaded', () => {                      // THIS EVENT IS TRIGGERED AS THE PAGE LOADS.
+
+    // ADD BUTTON //
+    const addButton = document.querySelector('#add-button');                // ADD BUTTON WILL BE OUR TRIGGER
+    addButton = document.querySelector ('click', addToPlaylist);
+})
+
+const addToPlaylist = function () {
+    // newSong.preventDefault ();
+    console.log('click');
+}
