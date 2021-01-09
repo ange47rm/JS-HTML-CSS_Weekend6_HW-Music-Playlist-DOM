e172790f6a72edc1917fd6a7488429fc26970b23
ange47rm <ange47rm@hotmail.com>
2021-01-09 14:39:27 +0000

    Now able to add items to the list

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 document.addEventListener( 'DOMContentLoaded', () => {                      // THIS EVENT IS TRIGGERED AS THE PAGE LOADS.
 
     // ADD BUTTON //
-    const addButton = document.querySelector('#add-button');                // (1) 'ADD BUTTON' WILL BE OUR TRIGGER
-    addButton.addEventListener ('click', addToPlaylist);                    // (2) EVENT LISTENER WILL BE A CLICK THAT WILL EXECUTE addToPlaylist
+    const addButton = document.querySelector('#new-song-form');             // (1) SELECT FORM OBJECT - THIS WILL BE OUR TRIGGER
+    addButton.addEventListener ('submit', addToPlaylist);                   // (2) TRIGGER WILL BE ITS SUBMISSION, THAT WILL EXECUTE addToPlaylist
 })
 
-const addToPlaylist = function (newSong) {                                  // newSong will be the user input (after form submission)
-    newSong.preventDefault ();
-    console.log('un pollo');
+const addToPlaylist = function (formInput) {                                // newSong will be the user input (after form submission)
+    formInput.preventDefault();
+    // CREATE NEW DOM OBJECT TO CAPTURE FORM INPUT //
+    const newSong = document.createElement('li');
+
+    
+    // MANIPULATE THE NEWLY CREATED OBJECT - HERE WE DEFINE WHAT IT WILL LOOK LIKE (TEXT) //
+    newSong.textContent = `${formInput.target.author.value} - ${formInput.target.songtitle.value}`
+
+
+    // GRAB THE HTML ELEMENT (in this case an 'ol') AND ATTACH NEW DOM OBJECT TO IT //
+    const songList = document.querySelector('ol');
+    songList.appendChild(newSong);
+    document.getElementById('new-song-form').reset();
 }
