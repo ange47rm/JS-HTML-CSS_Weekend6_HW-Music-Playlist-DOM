2a64aea1541a1322893f4b8de600e38aedb09638
ange47rm <ange47rm@hotmail.com>
2021-01-09 12:39:48 +0000

    Tested callback function reaction after triggering event using console.log

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 document.addEventListener( 'DOMContentLoaded', () => {                      // THIS EVENT IS TRIGGERED AS THE PAGE LOADS.
 
     // ADD BUTTON //
-    const addButton = document.querySelector('#add-button');                // ADD BUTTON WILL BE OUR TRIGGER
-    addButton = document.querySelector ('click', addToPlaylist);
+    const addButton = document.querySelector('#add-button');                // (1) 'ADD BUTTON' WILL BE OUR TRIGGER
+    addButton.addEventListener ('click', addToPlaylist);                    // (2) EVENT LISTENER WILL BE A CLICK THAT WILL EXECUTE addToPlaylist
 })
 
-const addToPlaylist = function () {
-    // newSong.preventDefault ();
-    console.log('click');
+const addToPlaylist = function (newSong) {                                  // newSong will be the user input (after form submission)
+    newSong.preventDefault ();
+    console.log('un pollo');
 }
